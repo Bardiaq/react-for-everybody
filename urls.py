48df0a4f58e646e88156860f883cdd175a3fc522
Bardia hasani <108074114+Bardiaq@users.noreply.github.com>
2022-07-29 02:06:49 +0430

    Update urls.py

--- urls.py
+++ urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from djangoProject4.GetData import getstudents
+from djangoProject4.GetData import getproducts
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('counterss/',getstudents),
+    path('counterss/',getproducts),
 ]
